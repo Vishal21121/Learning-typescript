f0ac0c7137744e3ff00983217b933cf56d6e5df8
Vishal21121 <vs84077000@gmail.com>
2022-11-13 18:24:33 +0530

    variables in typescript

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,14 @@
-const val = "Vishal"
-console.log(val)
+// const val = "Vishal"
+// console.log(val)
+
+// const hello = "world" // in typescript const works same as const in javascript but here in typescript we have data type for every variable hence for this const world is the data type and as const value cannot change hence anything we store in const becomes data type for const.
+
+let hello = "world"; // here the type of the hello variable becomes string as we have stored string to it
+// so we can change the value of  the hello to string only
+hello = "vishal"
+// but not other data type
+// hello = 1;
+
+// we can do the above thing by mentioning the data type explicitly
+let val : string = "world";
+
